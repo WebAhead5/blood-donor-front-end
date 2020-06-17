d88bfff1b5c825c3f76309a3c31ee3bc9805a67a
Marwanrizik91 <marwan.rizik@gmail.com>
2020-06-17 14:14:35 +0300

    built  WhiteBackgroundShadow component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import WhiteBackgroundShadow from '../whiteBackgroundShadow'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <WhiteBackgroundShadow />
+    
     </div>
   );
 }
