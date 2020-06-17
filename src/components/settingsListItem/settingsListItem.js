c1436f8d3c62a4044b8436f24195486f6acbef50
Marwanrizik91 <marwan.rizik@gmail.com>
2020-06-17 14:38:03 +0300

    build settings list item container

---
+++ src/components/settingsListItem/settingsListItem.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SettingsListItem = (props) => {
+     return (
+         <div className="settingsListItem_container">
+            {props.children}
+         </div>
+     )
+}
+
+export default SettingsListItem
