7548fe08d16337f71e5e78ee030e51f77c62f41d
Marwanrizik91 <marwan.rizik@gmail.com>
2020-06-18 15:43:42 +0300

    added languages support files and react-intl

--- src/index.js
+++ src/index.js
@@ -1,19 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/App';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { IntlProvider } from 'react-intl'
+import languages from './languages'
 
+
+const AppIndex = () => {
+
+  const [lang, setLang] = useState('en');
+  useEffect(() => setLang(lang), [lang]);
+
+  return (
+
+    <React.StrictMode>
+      <IntlProvider locale={lang} languages={languages[lang]}>
+        <Router>
+          <div style={{ direction: lang === 'ar' ? 'rtl' : 'ltr', fontFamily: 'Helvetica' }}>
+          <App setLang={setLang} lang={lang}/>
+          </div>
+        </Router>
+      </IntlProvider>
+    </React.StrictMode>
+  )
+}
 
 ReactDOM.render(
+  
 
-  <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
+  <AppIndex /> ,document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
