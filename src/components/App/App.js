8456424ff440acaa428b5d3fdc178e68d9f4da11
moris <moris.rafol@gmail.com>
2020-06-18 17:07:35 +0300

    fix: add react router to app.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,46 @@
 import React from "react";
 import "./App.css";
-import SettingsListScreen from "../screens/settingsListScreen";
-import NavBar from "../general/navBar";
-import HomeMenu from "../general/HomeMenu";
+import NavBar from "../components/general/navBar";
+import AlertList from "../components/general/alertList";
+import AlertListItem from "../components/general/alertMenuItem";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import SettingsListScreen from "../components/screens/settingsListScreen";
+import SubHeader from "../general/subHeader";
 
 function App() {
   return (
     <div>
-      <HomeMenu />
-      <SettingsListScreen />
+      {/*<Switch>*/}
+      {/*  <Route exact path="/">*/}
+      {/*    /!*render home screen*!/*/}
+      {/*  </Route>*/}
+
+      {/*  <Route exact path="/goals">*/}
+      {/*    /!*render home screen*!/*/}
+      {/*  </Route>*/}
+
+      {/*  <Route exact path="/personal">*/}
+      {/*    /!*render home screen*!/*/}
+      {/*  </Route>*/}
+
+      {/*  <Route exact path="/personal">*/}
+      {/*    /!*render home screen*!/*/}
+      {/*  </Route>*/}
+
+      {/*  <Route exact path="/settings">*/}
+      {/*    <SettingsListScreen />*/}
+      {/*  </Route>*/}
+
+      {/*  <Route exact path="/settings/personal">*/}
+      {/*    /!*render home screen*!/*/}
+      {/*  </Route>*/}
+
+      {/*  <Route exact path="/settings/notifications">*/}
+      {/*    /!*render home screen*!/*/}
+      {/*  </Route>*/}
+      {/*</Switch>*/}
+
+      <SubHeader />
       <NavBar />
     </div>
   );
