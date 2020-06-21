a330bcecf477c90dab664b58f29dd24e2ff2ced6
Marwanrizik91 <marwan.rizik@gmail.com>
2020-06-21 18:07:00 +0300

    adds personal settings input components, and personalSettings Screen.

--- src/components/general/personalSettingsInput/personalSettingsInput.js
+++ src/components/general/personalSettingsInput/personalSettingsInput.js
@@ -1,22 +1,22 @@
 import React from 'react'
+import WhiteBackgroundShodow from '../whiteBackgroundShadow'
 import './personalSettingsInput.css'
 
 const PersonalSettingsInput = (props) => {
 
     return (
-        <div className="personalSettingsInput_container">
+        <WhiteBackgroundShodow className={`personalSettingsInput_container ${props.className}`}>
 
-            <div className="personalSettingsInput_icon">
-                <img src={props.icon} alt={props.alt} />
+            <div className="personalSettingsInput_icon_container" >
+                <img className="personalSettingsInput_icon" src={props.icon} alt={props.alt} />
             </div>
-            <div className="personalSettingsInput_content">
-                {props.children ? props.children : <div>
-                    <label for='name' />
-                    <input type="text" value={props.value} className='personalSettingsInput_textContainer' /></div>
-                }
+            <div className='personalSettingsInput_vertical_line'></div>
+            <div className={`personalSettingsInput_content ${props.contentClassName}`}>
+                {/* Default is an input for the name */}
+                {props.children}
             </div>
 
-        </div>
+        </WhiteBackgroundShodow>
     )
 }
 
