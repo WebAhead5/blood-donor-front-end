692799b724d3f777253d528a4d7f18b6e148d194
Marwanrizik91 <marwan.rizik@gmail.com>
2020-06-22 11:02:35 +0300

    adds back button, fixes personalSettings style(height)

---
+++ src/components/general/backButton/backButton.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import './backButton.css'
+
+
+
+const BackButton = (props) => {
+
+    let history = useHistory();
+
+    function onClickGoBackArrow() {
+      
+        history.goBack();
+    }
+  
+    return (<img className="backButton_general"
+  
+      onClick={() => onClickGoBackArrow()}
+      alt="back button"
+      src="/img/icon-arrow.svg" />);
+  
+  }
+
+  export default BackButton
