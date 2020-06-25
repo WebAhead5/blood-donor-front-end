17a11937aa4c9c0728fd1bc3550f73e1d7433218
James Daniels <jrd656@gmail.com>
2020-06-25 13:46:02 +0100

    after moris check

--- src/components/screens/mapScreen/mapScreen.js
+++ src/components/screens/mapScreen/mapScreen.js
@@ -4,11 +4,28 @@ import MainScreenWrapper from "../../general/mainScreenWrapper";
 import TitleHeader from "../../general/titleHeader";
 import "./mapScreen.css"
 
-function MapScreen({arrayOfGeolocationObjects}) {
+function MapScreen({ arrayOfGeolocationObjects }) {
     return (
         <MainScreenWrapper className="mapScreen">
-            <TitleHeader title={"Blood Donation Locations"}/>
-            <MapBox arrayOfGeolocationObjects={arrayOfGeolocationObjects}/>
+            <TitleHeader title={"Blood Donation Locations"} />
+            <MapBox arrayOfGeolocationObjects={arrayOfGeolocationObjects} />
+            {/* <button
+                className="testShare"
+                onClick={async () => {
+                    if (navigator.share) {
+                        console.log("navigator.share is supported");
+                    } else {
+                        console.log("navigator.share is NOT supported")
+                    }
+                    try {
+                        await navigator.share({ title: "JD TITLE", text: "JD TEXT VALUE", url: "https://www.bbc.co.uk" })
+                        console.log("success in test share");
+                    }
+                    catch (err) {
+                        console.log("error in test Share", err.message);
+                    }
+                }}
+            ></button> */}
         </MainScreenWrapper>
     );
 }
