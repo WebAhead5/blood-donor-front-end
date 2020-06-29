caba4061cee5e1714f66b59066a315626c734eb2
Marwanrizik91 <marwan.rizik@gmail.com>
2020-06-29 11:00:05 +0300

    fixes he translation on notidication settings

--- src/languages/he.js
+++ src/languages/he.js
@@ -15,7 +15,7 @@ export default {
     SettingsContactUs: 'צור קשר',
     SettingsFAQ: 'שאלות נפוצות',
     SettingsAbout: 'אודות',
-    ReminderSettingsPhoneNotification: '?האם תרצה לקבל התראות לטלפון שלך',
+    ReminderSettingsPhoneNotification: 'האם תרצה לקבל התראות לטלפון שלך?',
     Yes: 'כן',
     No: 'לא',
     Home: 'דף בית',
