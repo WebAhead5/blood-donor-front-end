2985b5bef7f512104c923e4d3e24ae240bf42d00
James Daniels <jrd656@gmail.com>
2020-07-02 10:12:24 +0100

    some progress

---
+++ src/components/general/joinUsNow/joinUsNow.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function JoinUsNow(){
+
+
+    return (
+        <div className="joinUsNow">
+            <p>Join Us <span>NOW</span>! Donate <span>BLOOD</span> &amp; Save Lives</p>
+            <div className="triangleContainer">
+                <div>Triangle</div>
+            </div>
+        </div>
+    )
+}
+
+export default JoinUsNow
--- src/components/screens/homeScreen/homeScreen.js
+++ src/components/screens/homeScreen/homeScreen.js
@@ -3,12 +3,14 @@ import MainHeader from "../../general/mainHeader";
 import AlertList from "../../general/alertList";
 import MainScreenWrapper from "../../general/mainScreenWrapper";
 import HomeMenu from "../../general/homeMenu";
+import JoinUsNow from "../../general/joinUsNow";
 
 function HomeScreen({ alertsData, homeHeaderData }) {
   return (
     <MainScreenWrapper>
       <MainHeader />
       <HomeMenu data={homeHeaderData} />
+      <JoinUsNow/>
       <AlertList data={alertsData} />
     </MainScreenWrapper>
   );
