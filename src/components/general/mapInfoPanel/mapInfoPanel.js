bd27c9f61a54a05115d935eb786444212a4262ea
moris <moris.rafol@gmail.com>
2020-07-08 02:23:26 +0300

    refactor: move loading userSettings from local storage to App.js feat: filter alerts in App.js

--- src/components/general/mapInfoPanel/mapInfoPanel.js
+++ src/components/general/mapInfoPanel/mapInfoPanel.js
@@ -4,10 +4,15 @@ import React, {useState} from 'react';
 import AddToCalendar from '../addToCalendar';
 import {FormattedMessage, injectIntl} from 'react-intl'
 import Share from '../share'
+import {
+    isAndroid,
+    isIOS,
+    isBrowser,
+    isMobile
+} from "react-device-detect";
 
-
-function MapInfoPanel({selectedLocation: {Name, City, DateDonation, FromHour, ToHour}={}, intl}) {
-    const address =  Name + " " + City;
+function MapInfoPanel({selectedLocation: {Name, City, DateDonation, FromHour, ToHour,long,lat} = {}, intl}) {
+    const address = Name + ", " + City + ", " + "israel";
     const textToShare = {
         title: "come donate blood at...",
         body: "....."
@@ -16,13 +21,25 @@ function MapInfoPanel({selectedLocation: {Name, City, DateDonation, FromHour, To
         title: "Blood Donation",
         description: "Reminder to donate blood",
         location: address,
-        startTime: moment(DateDonation).hours(FromHour.slice(0,2)).minutes(FromHour.slice(3,5)).toDate(),
-        endTime: moment(DateDonation).hours(ToHour.slice(0,2)).minutes(ToHour.slice(3,5)).toDate(),
+        startTime: moment(DateDonation).hours(FromHour.slice(0, 2)).minutes(FromHour.slice(3, 5)).toDate(),
+        endTime: moment(DateDonation).hours(ToHour.slice(0, 2)).minutes(ToHour.slice(3, 5)).toDate(),
     }
 
     const [shareHidden, setShareHidden] = useState(true)
 
-    
+    function navigationButton() {
+
+        if (isBrowser)
+            return <button className="infoPanel_button infoPanel_go" onClick={()=>window.open(`https://www.google.com/maps/dir/?api=1&destination=${encodeURI(address)}`)}>go</button>
+
+        else if (isMobile)
+            if (isAndroid)
+                return <button className="infoPanel_button infoPanel_go" onClick={()=>window.open(`geo:?q=${encodeURI(address)}`)}>Go</button>
+            else if (isIOS)
+                return <button className="infoPanel_button infoPanel_go" onClick={()=>window.open(`http://maps.apple.com/?q=${encodeURI(address)}&sll=${long},${lat}`)}>go</button>
+
+    }
+
     return (
 
         <div className="infoPanel">
@@ -48,14 +65,13 @@ function MapInfoPanel({selectedLocation: {Name, City, DateDonation, FromHour, To
                 <AddToCalendar
                     buttonLabel={intl.formatMessage({id: 'AddToCalendar'})}
                     event={addToCalenderEvent}
-                    className="infoPanel_button infoPanel_shareButton"
+                    className="infoPanel_button infoPanel_addToCalender"
                 />
-
+                {navigationButton()}
 
             </div>
 
             {!shareHidden ? <Share shareData={textToShare} onCloseClick={() => setShareHidden(true)}/> : null}
-
         </div>
     )
 }
